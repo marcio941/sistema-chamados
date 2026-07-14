1746f8e1a1cb5db0dadfe00ff01ee16c4ed9a4d2
marcio941 <marciooliveira941@gmail.com>
2026-07-13 23:34:15 -0300

    Modify SUPABASE_URL in auth-guard.js
    Updated SUPABASE_URL to remove the '/rest/v1/' suffix.

--- auth-guard.js
+++ auth-guard.js
@@ -6,7 +6,7 @@
 // <script src="auth-guard.js"></script>
 
 (function () {
-  const SUPABASE_URL = 'https://brlprkcmxxoiyxmcapww.supabase.co/rest/v1/';
+  const SUPABASE_URL = 'https://brlprkcmxxoiyxmcapww.supabase.co';
   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJybHBya2NteHhvaXl4bWNhcHd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MjEyNDUsImV4cCI6MjA5MDM5NzI0NX0.e94z1xlTNzsIcwhGamlK1b5CE4Kx2rk1g1fdmUgHKKM';
 
   // Mesma lista de restrição usada no login.html
